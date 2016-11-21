b7f2a969a47fe1d2031fd6537b339e526df21684
jecourtney <JCourtney@unitedmcgill.com>
2016-11-21 10:33:57 -0500

    commit

---
+++ Test.cs
@@ -0,0 +1,11 @@
+namespace McGill.Web
+{
+    public class DoMathThings
+    {
+        public int AddTwoNumbers(int num1, int num2)
+        {
+            // This is a test
+            return num1 + num2;
+        }
+    }
+}
